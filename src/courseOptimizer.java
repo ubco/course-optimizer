40fc516cd25cfcbfa66a18710bb256cdd72a405a
Kevin Van Kessel <kevin.g.vankessel@gmail.com>
2015-12-27 18:19:27 -0800

    Added TimeTable class

--- src/courseOptimizer.java
+++ src/courseOptimizer.java
@@ -3,5 +3,8 @@ import java.util.ArrayList;
 public class courseOptimizer {
 	public static void main(String[] args) {
 		ArrayList<Period>[] timeTable = new ArrayList[7];
+		for(int i = 0; i < 7; i++) {
+			timeTable[i] = new ArrayList<Period>();
+		}
 	}
 }
